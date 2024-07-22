7bdba5fbe4446a6035de212e8c0a3dab4982f6e2
Aryan Garg <gargaryan277@gmail.com>
2024-07-22 21:14:14 +0530

    updated question

--- src/Days_100/binarySearch.java
+++ src/Days_100/binarySearch.java
@@ -6,5 +6,6 @@ public class binarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] nums = new int[n];
     }
 }
