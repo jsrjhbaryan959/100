7fb4fd786444fdf1a5303b33ad6d6f8a88de1dd4
Aryan Garg <gargaryan277@gmail.com>
2024-07-22 21:38:01 +0530

    updated question

--- src/Days_100/binarySearch.java
+++ src/Days_100/binarySearch.java
@@ -11,5 +11,26 @@ public class binarySearch {
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
+
+        int target = sc.nextInt();
+
+        int start = 0;
+        int end = n-1;
+        int mid;
+
+        while (start <= end){
+            mid =(start+end)/2;
+            if(target == nums[mid]){
+                System.out.println(mid);
+                break;
+            }
+            else if (target > nums[mid]) {
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+        System.out.println(-1);
     }
 }
