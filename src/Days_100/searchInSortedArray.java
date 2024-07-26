4085e8ba28dc8e7f03e473a0ed3592775912f4ad
Aryan Garg <gargaryan277@gmail.com>
2024-07-26 22:29:02 +0530

    updated question 23

--- src/Days_100/searchInSortedArray.java
+++ src/Days_100/searchInSortedArray.java
@@ -1,34 +1,42 @@
 package Days_100;
 
+import java.util.Scanner;
+
 public class searchInSortedArray {
     public static void main(String[] args) {
-        class Solution {
-            public int search(int[] nums, int target) {
-                int start = 0 ;
-                int end = nums.length-1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
 
-                while(start<=end){
-                    int mid = (start + end)/2;
-                    if(nums[mid]==target){
-                        return mid ;
-                    }else if (nums[start]<=nums[mid]){ // first part is sorted
+        int start = 0;
+        int end = nums.length-1;
+        while(start <= end){
+            int mid = (start + end)/2;
 
-                        if(target>=nums[start] && target<nums[mid]){
-                            end = mid-1;
-                        }else{
-                            start = mid+1;
-                        }
-
-                    }else{ // second part is sorted
-                        if(target>nums[mid] && target<=nums[end]){
-                            start = mid+1;
-                        }else{
-                            end = mid-1;
-                        }
-                    }
+            if(nums[mid] == target){
+                System.out.println(mid);
+            }
+            else if(nums[start] <= nums[mid]){
+                if(target >= nums[start] && target < nums[mid]){
+                    end = mid-1;
                 }
-                return -1 ;
+                else{
+                    start = mid+1;
+                }
+            }
+            else{
+                if(target > nums[mid] && target <= nums[end]){
+                    start = mid+1;
+                }
+                else{
+                    end = mid-1;
+                }
             }
         }
+        System.out.println(-1);
     }
 }
