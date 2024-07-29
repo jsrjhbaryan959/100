8124a9f4523dc17637e5f448c8a97045d798a057
Aryan Garg <gargaryan277@gmail.com>
2024-07-29 21:46:18 +0530

    updated question 28

---
+++ src/Days_100/sortColors.java
@@ -0,0 +1,46 @@
+package Days_100;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sortColors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+
+        while(mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, high, mid);
+                high--;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+
+}
