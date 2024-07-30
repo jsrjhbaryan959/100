c154e8e1bedf02b91dbd7cb722e905b339819969
Aryan Garg <gargaryan277@gmail.com>
2024-07-30 22:20:00 +0530

    updated question 29

---
+++ src/Days_100/maximumSubarray.java
@@ -0,0 +1,27 @@
+package Days_100;
+
+import java.util.Scanner;
+
+public class maximumSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+
+        int currentsum = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            if(currentsum + nums[i] > nums[i]){
+                currentsum+=nums[i];
+            }
+            else{
+                currentsum=nums[i];
+            }
+            max = Math.max(currentsum, max);
+        }
+        System.out.println(max);
+    }
+}
