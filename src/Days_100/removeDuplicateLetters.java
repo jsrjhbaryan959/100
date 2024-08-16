7df0a23bbd69f2a3df82171317c1e6b99d5a5327
Aryan Garg <gargaryan277@gmail.com>
2024-08-16 23:16:01 +0530

    updated question 46

---
+++ src/Days_100/removeDuplicateLetters.java
@@ -0,0 +1,53 @@
+package Days_100;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class removeDuplicateLetters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+
+        class Solution {
+            public String removeDuplicateLetters(String s) {
+                int[] lastIndex = new int[26];
+
+                for(int i=0;i<s.length();i++){
+                    char ch = s.charAt(i);
+                    int idx = (int)(ch-'a');
+
+                    lastIndex[idx]= i ;
+                }
+
+                boolean[] present = new boolean[26];
+
+                Stack<Character> st = new Stack<>();
+
+                for(int i=0;i<s.length();i++){
+                    char ch = s.charAt(i);
+                    int idx = (int)(ch-'a');
+
+                    if(present[idx]==false){
+                        while(st.size()>0 && st.peek()>ch && lastIndex[(int)(st.peek()-'a')]>i){
+                            present[(int)(st.peek()-'a')]=false;
+                            st.pop();
+                        }
+
+                        st.push(ch);
+                        present[idx]=true ;
+                    }
+                }
+
+                StringBuilder sb = new StringBuilder("");
+
+                while(st.size()>0){
+                    sb.append(st.pop());
+                }
+
+                return sb.reverse().toString();
+            }
+        }
+
+    }
+}
