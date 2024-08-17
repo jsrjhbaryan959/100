d07b544c98bb08e94cb596ef4f94cec6db8688d2
Aryan Garg <gargaryan277@gmail.com>
2024-08-17 22:05:55 +0530

    new updated question 46

--- src/Days_100/removeDuplicateLetters.java
+++ src/Days_100/removeDuplicateLetters.java
@@ -8,46 +8,34 @@ public class removeDuplicateLetters {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
 
+        int[] lastIndex = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int index = (int)(ch - 'a');
 
-        class Solution {
-            public String removeDuplicateLetters(String s) {
-                int[] lastIndex = new int[26];
-
-                for(int i=0;i<s.length();i++){
-                    char ch = s.charAt(i);
-                    int idx = (int)(ch-'a');
-
-                    lastIndex[idx]= i ;
-                }
-
-                boolean[] present = new boolean[26];
-
-                Stack<Character> st = new Stack<>();
-
-                for(int i=0;i<s.length();i++){
-                    char ch = s.charAt(i);
-                    int idx = (int)(ch-'a');
-
-                    if(present[idx]==false){
-                        while(st.size()>0 && st.peek()>ch && lastIndex[(int)(st.peek()-'a')]>i){
-                            present[(int)(st.peek()-'a')]=false;
-                            st.pop();
-                        }
-
-                        st.push(ch);
-                        present[idx]=true ;
-                    }
-                }
-
-                StringBuilder sb = new StringBuilder("");
-
-                while(st.size()>0){
-                    sb.append(st.pop());
-                }
-
-                return sb.reverse().toString();
-            }
+            lastIndex[index] = i;
         }
 
+        boolean[] present = new boolean[26];
+
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int index = (int) (ch - 'a');
+
+            if (present[index] == false) {
+                while (st.size() > 0 && st.peek() > ch && (lastIndex[(int) st.peek() - 'a'] > i)) {
+                    present[(int) st.peek() - 'a'] = false;
+                    st.pop();
+                }
+                st.push(ch);
+                present[index] = true;
+            }
+        }
+        StringBuilder sb = new StringBuilder("");
+        while (st.size() > 0){
+            sb.append(st.pop());
+        }
+        System.out.println(sb.reverse().toString());
     }
 }
