01e5089e53ccd43931bf40e0680bdc49fb3c6c20
Aryan Garg <gargaryan277@gmail.com>
2024-08-29 23:39:36 +0530

    updated question 58

---
+++ src/Days_100/spiralMatrixII.java
@@ -0,0 +1,11 @@
+package Days_100;
+
+import java.util.Scanner;
+
+public class spiralMatrixII {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+    }
+}
