191d1df2418381e6f4705dc17ead1edb2cef427c
Aryan Garg <gargaryan277@gmail.com>
2024-09-01 22:13:01 +0530

    updated question 61

---
+++ src/Days_100/palindromeLinkedList.java
@@ -0,0 +1,30 @@
+package Days_100;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class palindromeLinkedList {
+    ListNode head;
+
+    Stack<Integer> st = new Stack<>();
+
+    ListNode ptr = head;
+
+//    while (ptr ! = null){
+//        st.push(ptr.val);
+//        ptr = ptr.next;
+//    }
+
+    ListNode ptr2 = head;
+//    while(ptr2 != null){
+//        int val1 = ptr2.val;
+//        int val2 = st.pop();
+//
+//        if(val1 != val2){
+//           System.out.println("false");
+//        }
+//        ptr2 = ptr2.next;
+//        System.out.println("true");
+//    }
+}
+
