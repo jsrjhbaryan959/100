ce8a150b92cdcaea602d3845011c267429eeb692
Aryan Garg <gargaryan277@gmail.com>
2024-09-01 21:37:31 +0530

    new updated question 56

--- src/Days_100/matrixDiagonalSum.java
+++ src/Days_100/matrixDiagonalSum.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class matrixDiagonalSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] matrix = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int sum = 0;
+        int len = matrix.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if(i==j || i+j == len-1){
+                    sum+=matrix[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
